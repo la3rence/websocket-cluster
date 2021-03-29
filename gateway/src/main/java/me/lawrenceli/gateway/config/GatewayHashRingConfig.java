3e8d75859f4c1838813ed9b48a2aaec34afb384c
Lawrence Li <lonor@live.com>
2021-03-29 17:53:21 +0800

    lb: refactor with the new reactive load balance filter

--- gateway/src/main/java/me/lawrenceli/gateway/config/GatewayHashRingConfig.java
+++ gateway/src/main/java/me/lawrenceli/gateway/config/GatewayHashRingConfig.java
@@ -1,7 +1,7 @@
 package me.lawrenceli.gateway.config;
 
 import me.lawrenceli.contant.GlobalConstant;
-import me.lawrenceli.gateway.filter.CustomLoadBalanceFilter;
+import me.lawrenceli.gateway.filter.CustomReactiveLoadBalanceFilter;
 import me.lawrenceli.gateway.server.ServiceNode;
 import me.lawrenceli.hashring.ConsistentHashRouter;
 import me.lawrenceli.hashring.VirtualNode;
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.gateway.config.LoadBalancerProperties;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -31,28 +32,31 @@ public class GatewayHashRingConfig {
 
     final RedisTemplate<Object, Object> redisTemplate;
     final WebSocketProperties webSocketProperties;
+    private final LoadBalancerClientFactory clientFactory;
 
-    public GatewayHashRingConfig(RedisTemplate<Object, Object> redisTemplate, WebSocketProperties webSocketProperties) {
+    public GatewayHashRingConfig(RedisTemplate<Object, Object> redisTemplate,
+                                 WebSocketProperties webSocketProperties,
+                                 LoadBalancerClientFactory clientFactory) {
         this.redisTemplate = redisTemplate;
         this.webSocketProperties = webSocketProperties;
+        this.clientFactory = clientFactory;
     }
 
     /**
-     * 初始化自定义负载均衡过滤器
-     *
-     * @param client               bean
-     * @param properties           bean
-     * @param consistentHashRouter {@link #init() init方法}注入，此处未使用构造注入（会产生循环依赖）
-     * @return CustomLoadBalanceFilter bean
-     * @see CustomLoadBalanceFilter
+     * @param client                 负载均衡客户端
+     * @param loadBalancerProperties 负载均衡配置
+     * @param consistentHashRouter   {@link #init() init方法}注入，此处未使用构造注入（会产生循环依赖）
+     * @param discoveryClient        服务发现客户端
+     * @return 注入自定义的 Reactive 过滤器 Bean 对象
      */
     @Bean
-    public CustomLoadBalanceFilter customLoadBalanceFilter(LoadBalancerClient client,
-                                                           LoadBalancerProperties properties,
-                                                           ConsistentHashRouter<ServiceNode> consistentHashRouter,
-                                                           DiscoveryClient discoveryClient) {
-        logger.debug("初始化 CustomLoadBalanceFilter: {}, {}", client, properties);
-        return new CustomLoadBalanceFilter(client, properties, consistentHashRouter, webSocketProperties, discoveryClient);
+    public CustomReactiveLoadBalanceFilter customReactiveLoadBalanceFilter(LoadBalancerClient client,
+                                                                           LoadBalancerProperties loadBalancerProperties,
+                                                                           ConsistentHashRouter<ServiceNode> consistentHashRouter,
+                                                                           DiscoveryClient discoveryClient) {
+        logger.debug("初始化 自定义响应式负载均衡器: {}, {}", client, loadBalancerProperties);
+        return new CustomReactiveLoadBalanceFilter(clientFactory, loadBalancerProperties,
+                consistentHashRouter, discoveryClient, webSocketProperties);
     }
 
     @Bean
--- gateway/src/main/java/me/lawrenceli/gateway/filter/CustomLoadBalanceFilter.java
+++ gateway/src/main/java/me/lawrenceli/gateway/filter/CustomLoadBalanceFilter.java
@@ -1,11 +1,8 @@
 package me.lawrenceli.gateway.filter;
 
-import me.lawrenceli.contant.GlobalConstant;
 import me.lawrenceli.gateway.config.WebSocketProperties;
 import me.lawrenceli.gateway.server.ServiceNode;
 import me.lawrenceli.hashring.ConsistentHashRouter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -13,28 +10,30 @@ import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.gateway.config.LoadBalancerProperties;
 import org.springframework.cloud.gateway.filter.LoadBalancerClientFilter;
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
-import org.springframework.http.server.PathContainer;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 
 import java.net.URI;
 import java.util.List;
 
 /**
- * 自定义负载均衡策略
+ * 已废弃的自定义负载均衡过滤器
+ * - 由于旧的 LoadBalancerClientFilter 已不推荐使用，此旧实现保留，简单易懂，原理不变。
  *
  * @author lawrence
  * @since 2021/3/24
+ * @deprecated 推荐使用响应式网关的新过滤器 {@link CustomReactiveLoadBalanceFilter}
  */
 public class CustomLoadBalanceFilter extends LoadBalancerClientFilter implements BeanPostProcessor {
-
-    private static final Logger logger = LoggerFactory.getLogger(CustomLoadBalanceFilter.class);
 
     final ConsistentHashRouter<ServiceNode> consistentHashRouter;
     final WebSocketProperties webSocketProperties;
     final DiscoveryClient discoveryClient;
 
-    public CustomLoadBalanceFilter(LoadBalancerClient loadBalancer, LoadBalancerProperties properties, ConsistentHashRouter<ServiceNode> consistentHashRouter, WebSocketProperties webSocketProperties, DiscoveryClient discoveryClient) {
+    public CustomLoadBalanceFilter(LoadBalancerClient loadBalancer,
+                                   LoadBalancerProperties properties,
+                                   ConsistentHashRouter<ServiceNode> consistentHashRouter,
+                                   WebSocketProperties webSocketProperties,
+                                   DiscoveryClient discoveryClient) {
         super(loadBalancer, properties);
         this.consistentHashRouter = consistentHashRouter;
         this.webSocketProperties = webSocketProperties;
@@ -47,23 +46,7 @@ public class CustomLoadBalanceFilter extends LoadBalancerClientFilter implements
         String instancesId = originalUrl.getHost();
         if (webSocketProperties.getService().getName().equals(instancesId)) {
             // 获取需要参与哈希的字段，此项目为 userId
-            String userId = null;
-            if (originalUrl.getPath().startsWith(GlobalConstant.WEBSOCKET_ENDPOINT_PATH)) {
-                // ws: "lb://websocket-server/connect/1" 获取这里面的最后一个路径参数 userId: 1
-                List<PathContainer.Element> elements = exchange.getRequest().getPath().elements();
-                PathContainer.Element lastElement = elements.get(elements.size() - 1);
-                userId = lastElement.value();
-                logger.debug("【网关负载均衡过滤器】WebSocket 获取到 userId: {}", userId);
-            } else {
-                // 前提：websocket http 服务 userId 放在 query 中
-                // rest: "lb://websocket-server/send?userId=1&message=text"
-                MultiValueMap<String, String> queryParams = exchange.getRequest().getQueryParams();
-                List<String> userIds = queryParams.get(GlobalConstant.KEY_TO_BE_HASHED);
-                if (null != userIds && !userIds.isEmpty()) {
-                    userId = userIds.get(0);
-                    logger.debug("【网关负载均衡过滤器】HTTP 获取到 userId: {}", userId);
-                }
-            }
+            final String userId = WebSocketSessionLoadBalancer.getUserIdFromRequest(exchange);
             if (null != userId) {
                 // 请求参数中有 userId，需要经过哈希环的路由
                 ServiceNode serviceNode = consistentHashRouter.routeNode(userId);
@@ -73,7 +56,6 @@ public class CustomLoadBalanceFilter extends LoadBalancerClientFilter implements
                     for (ServiceInstance instance : instances) {
                         // 如果 userId 映射后的真实节点的 IP 与某个实例 IP 一致，就转发
                         if (instance.getHost().equals(serviceNode.getKey())) {
-                            logger.debug("当前客户端[{}]匹配到真实节点 {}", userId, serviceNode.getKey());
                             return instance;
                         }
                     }
