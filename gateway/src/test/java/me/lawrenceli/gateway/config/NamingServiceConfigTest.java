cb4355e7f0f25fbea8af9f6a8ac1fc94f010ffc6
Lawrence Li <lonor@live.com>
2021-08-15 16:36:50 +0800

    test: add configuration test
    Signed-off-by: Lawrence Li <lonor@live.com>

---
+++ gateway/src/test/java/me/lawrenceli/gateway/config/NamingServiceConfigTest.java
@@ -0,0 +1,35 @@
+package me.lawrenceli.gateway.config;
+
+import com.alibaba.nacos.client.naming.NacosNamingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * @author lawrence
+ * @since 2021/8/15
+ */
+class NamingServiceConfigTest {
+
+    static WebSocketProperties webSocketProperties;
+
+    static {
+        webSocketProperties = new WebSocketProperties();
+        webSocketProperties.setNacosServerAddress("127.0.0.1");
+        webSocketProperties.setNacosNamespace("test");
+    }
+
+    private NamingServiceConfig namingServiceConfig;
+
+    @BeforeEach
+    void setUp() {
+        namingServiceConfig = new NamingServiceConfig(webSocketProperties);
+    }
+
+    @Test
+    void getNamingService() {
+        NacosNamingService namingService = namingServiceConfig.getNamingService();
+        assertNotNull(namingService);
+    }
+}
