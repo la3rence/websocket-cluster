dae28c1b4a421ee0778cc2caa2bb65493084bfbc
Lawrence Li <lonor@live.com>
2021-08-19 22:27:46 +0800

    test: add server status change tests
    Signed-off-by: Lawrence Li <lonor@live.com>

--- gateway/src/main/java/me/lawrenceli/gateway/server/RedisSubscriber.java
+++ gateway/src/main/java/me/lawrenceli/gateway/server/RedisSubscriber.java
@@ -32,9 +32,9 @@ public class RedisSubscriber {
 
     final DiscoveryClient discoveryClient; // spring cloud native interface
     final WebSocketProperties webSocketProperties;
-    final ConsistentHashRouter<ServiceNode> consistentHashRouter;
     final RedisTemplate<Object, Object> redisTemplate;
     final FanoutSender fanoutSender;
+    private ConsistentHashRouter<ServiceNode> consistentHashRouter;
 
     public RedisSubscriber(DiscoveryClient discoveryClient,
                            WebSocketProperties webSocketProperties,
@@ -45,6 +45,10 @@ public class RedisSubscriber {
         this.consistentHashRouter = consistentHashRouter;
         this.redisTemplate = redisTemplate;
         this.fanoutSender = fanoutSender;
+    }
+
+    public void setConsistentHashRouter(ConsistentHashRouter<ServiceNode> consistentHashRouter) {
+        this.consistentHashRouter = consistentHashRouter;
     }
 
     /**
@@ -61,15 +65,15 @@ public class RedisSubscriber {
         String upOrDown = webSocketMessage.getContent();
         // 实例的标识：IP
         String serverIp = webSocketMessage.getServerIp();
-        logger.info("【哈希环】该实例上线之前为 {}, 稍后将更新...", JSON.toJSONString(consistentHashRouter.getRing()));
+        logger.info("【哈希环】该实例上线之前为 {}, 稍后将更新...", JSON.toJSONString(consistentHashRouter.getRing())); // NOSONAR
         if (GlobalConstant.SERVER_UP_MESSAGE.equalsIgnoreCase(upOrDown)) {
             // 实例上线, 但 Nacos 可能尚未发现服务，此处再等 Nacos 获取到最新服务列表
-            Thread.sleep(5000);
+            Thread.sleep(3000);
             // 一个服务上线了，应当告知原本哈希到其他节点但现在路由到此节点的所有客户端断开连接
             // 为了确定是哪些客户端需要重连，可以遍历所有 userId 和哈希，筛选出节点添加前后匹配到不同真实节点的所有 userId
             // 因此，每次 WebSocket 有新连接(onOpen)的时候都有必要将 userId(+hash) 保存在 redis 中，然后在节点变动时取出
             Map<Object, Object> userIdAndHashInRedis = redisTemplate.opsForHash().entries(GlobalConstant.KEY_TO_BE_HASHED);
-            logger.debug("Redis 中 userId hash : {}", JSON.toJSONString(userIdAndHashInRedis));
+            logger.debug("Redis 中 userId hash : {}", userIdAndHashInRedis);
             Map<String, ServiceNode> oldUserAndServer = new ConcurrentHashMap<>();
             for (Object userIdObj : userIdAndHashInRedis.keySet()) {
                 String userId = (String) userIdObj;
@@ -87,13 +91,13 @@ public class RedisSubscriber {
             consistentHashRouter.addNode(serviceNode, GlobalConstant.VIRTUAL_COUNT);
             // 添加了 node 之后就可能有部分 userId 路由到的真实服务节点发生变动
             List<String> userIdClientsToReset = new ArrayList<>();
-            for (String userId : oldUserAndServer.keySet()) {
-                ServiceNode newServiceNode = consistentHashRouter.routeNode(userId);
-                logger.debug("【遍历】当前客户端 [{}] 的新节点 [{}]", userId, newServiceNode);
+            for (Map.Entry<String, ServiceNode> entry : oldUserAndServer.entrySet()) {
+                ServiceNode newServiceNode = consistentHashRouter.routeNode(entry.getKey());
+                logger.debug("【遍历】当前客户端 [{}] 的新节点 [{}]", entry.getKey(), newServiceNode);
                 // 同一 userId 路由到的真实服务节点前后可能会不一样, 把这些 userId 筛选出来
-                if (!newServiceNode.getKey().equals(oldUserAndServer.get(userId).getKey())) {
-                    userIdClientsToReset.add(userId);
-                    logger.info("【哈希环更新】客户端在哈希环的映射服务节点发生了变动: [{}]: [{}] -> [{}]", userId, oldUserAndServer.get(userId), newServiceNode);
+                if (!newServiceNode.getKey().equals(entry.getValue().getKey())) {
+                    userIdClientsToReset.add(entry.getKey());
+                    logger.info("【哈希环更新】客户端在哈希环的映射服务节点发生了变动: [{}]: [{}] -> [{}]", entry.getKey(), entry.getValue(), newServiceNode);
                 }
             }
             // 通知部分客户端断开连接, 可以发一个全局广播让客户端断开，也可以服务端主动断开，其实就是这些客户端都要自动连接上新的实例
@@ -107,6 +111,6 @@ public class RedisSubscriber {
         // 将最新的哈希环放到 Redis
         SortedMap<Long, VirtualNode<ServiceNode>> ring = consistentHashRouter.getRing();
         redisTemplate.opsForHash().putAll(GlobalConstant.HASH_RING_REDIS, ring);
-        logger.info("【哈希环】实例上线之后为 {}", JSON.toJSONString(ring));
+        logger.info("【哈希环】实例上线之后为 {}", JSON.toJSONString(ring)); // NOSONAR
     }
 }
--- gateway/src/main/java/me/lawrenceli/gateway/server/ServiceNode.java
+++ gateway/src/main/java/me/lawrenceli/gateway/server/ServiceNode.java
@@ -3,6 +3,7 @@ package me.lawrenceli.gateway.server;
 import me.lawrenceli.hashring.Node;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * 真实节点
@@ -30,5 +31,18 @@ public class ServiceNode implements Node, Serializable {
     @Override
     public String toString() {
         return getKey();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ServiceNode that = (ServiceNode) o;
+        return Objects.equals(ip, that.ip);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ip);
     }
 }
